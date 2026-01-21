4d5ea1bd4eb419a2fb8a7a285eb12c79e32a511d
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2026-01-21 16:22:33 -0600

    switching to print count

--- src/mujoco_system_interface.cpp
+++ src/mujoco_system_interface.cpp
@@ -871,18 +871,19 @@ MujocoSystemInterface::on_init(const hardware_interface::HardwareComponentInterf
       std::make_shared<realtime_tools::RealtimePublisher<sensor_msgs::msg::JointState>>(actuator_state_publisher_);
 
   // We explicitly check to make sure that all joints have names, otherwise stuff down the line won't work
-  bool all_joints_with_names = true;
+  int num_joints_without_name = 0;
   for (int i = 0; i < mj_model_->njnt; ++i)
   {
     const char* joint_name = mj_id2name(mj_model_, mjtObj::mjOBJ_JOINT, i);
     if (!joint_name)
     {
-      RCLCPP_FATAL(get_logger(), "Joint %d does not have a name. All joints must have names.", i);
-      all_joints_with_names = false;
+      num_joints_without_name++;
     }
   }
-  if (!all_joints_with_names)
+  if (num_joints_without_name)
   {
+    RCLCPP_FATAL(get_logger(), "%d joints in the mjcf don't have names. All joints must have names.",
+                 num_joints_without_name);
     return hardware_interface::CallbackReturn::FAILURE;
   }
 
