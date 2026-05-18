754a3fd268fd2fa16721f63a68a7dea38d580a3c
Misha Savchenko <misha.savchenko@nasa.gov>
2026-05-18 12:38:12 -0500

    trying out this egl context

--- mujoco_ros2_control/src/mujoco_cameras.cpp
+++ mujoco_ros2_control/src/mujoco_cameras.cpp
@@ -22,6 +22,8 @@
 
 #include "sensor_msgs/image_encodings.hpp"
 
+#include <EGL/egl.h>
+
 using namespace std::chrono_literals;
 
 namespace mujoco_ros2_control
@@ -152,6 +154,58 @@ void MujocoCameras::close()
 
 void MujocoCameras::update_loop()
 {
+  {
+        // desired config
+    const EGLint configAttribs[] = {
+      EGL_RED_SIZE,           8,
+      EGL_GREEN_SIZE,         8,
+      EGL_BLUE_SIZE,          8,
+      EGL_ALPHA_SIZE,         8,
+      EGL_DEPTH_SIZE,         24,
+      EGL_STENCIL_SIZE,       8,
+      EGL_COLOR_BUFFER_TYPE,  EGL_RGB_BUFFER,
+      EGL_SURFACE_TYPE,       EGL_PBUFFER_BIT,
+      EGL_RENDERABLE_TYPE,    EGL_OPENGL_BIT,
+      EGL_NONE
+    };
+
+    // get default display
+    EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (eglDpy==EGL_NO_DISPLAY) {
+      mju_error("Could not get EGL display, error 0x%x\n", eglGetError());
+    }
+
+    // initialize
+    EGLint major, minor;
+    if (eglInitialize(eglDpy, &major, &minor)!=EGL_TRUE) {
+      mju_error("Could not initialize EGL, error 0x%x\n", eglGetError());
+    }
+
+    // choose config
+    EGLint numConfigs;
+    EGLConfig eglCfg;
+    if (eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs)!=EGL_TRUE) {
+      mju_error("Could not choose EGL config, error 0x%x\n", eglGetError());
+    }
+
+    // bind OpenGL API
+    if (eglBindAPI(EGL_OPENGL_API)!=EGL_TRUE) {
+      mju_error("Could not bind EGL OpenGL API, error 0x%x\n", eglGetError());
+    }
+
+    // create context
+    EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
+    if (eglCtx==EGL_NO_CONTEXT) {
+      mju_error("Could not create EGL context, error 0x%x\n", eglGetError());
+    }
+
+    // make context current, no surface (let OpenGL handle FBO)
+    if (eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, eglCtx)!=EGL_TRUE) {
+      mju_error("Could not make EGL context current, error 0x%x\n", eglGetError());
+    }
+
+  }
+
   // {
   //   // We create an offscreen context specific to this process for managing camera rendering.
   //   glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
